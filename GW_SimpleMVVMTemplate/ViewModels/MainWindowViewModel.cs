29e367d9ed91c70fe1b846cf5cb8990d1e6ea304
Guanwei Su <guanweisu514@gmail.com>
2022-11-14 20:15:56 +0800

    加入專案檔案。

---
+++ GW_SimpleMVVMTemplate/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,56 @@
+﻿using GW_SimpleMVVMTemplate.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Markup;
+
+namespace GW_SimpleMVVMTemplate.ViewModels
+{
+    /// <summary>
+    /// A Simple MVVM Template Created by GuanWei,Su. 2022/11/14
+    /// </summary>
+    public class MainWindowViewModel : ViewModelBase, IDisposable
+    {
+        public MainWindowViewModel()
+        {
+        }
+        #region Properties
+        private string _HelloString = string.Empty;
+        public string HelloString { get { return _HelloString; } set { _HelloString = value; NotifyPropertyChanged(); } }
+        #endregion
+        #region ViewLoaded
+        public ICommand ViewLoaded { get { return new RelayCommand(param => ViewLoadedExecute(), param => true); } }
+        private void ViewLoadedExecute()
+        {
+        }
+        #endregion
+        #region ViewModelDispose
+        public void Dispose()
+        {
+        }
+        #endregion
+        #region HelloButtonClick
+        public ICommand HelloButtonClick { get { return new RelayCommand(param => HelloButtonClickExecute(), param => true); } }
+        private void HelloButtonClickExecute()
+        {
+            if (string.IsNullOrEmpty(HelloString))
+            {
+                HelloString = "Hello My Friend!";
+            }
+            else
+            {
+                HelloString = string.Empty;
+            }
+
+            //App.Current.Dispatcher.Invoke((Action)delegate
+            //{
+            //    HelloString = string.Empty;
+            //});
+        }
+        #endregion
+    }
+}
---
+++ GW_SimpleMVVMTemplate/ViewModels/ViewModelBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GW_SimpleMVVMTemplate.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        #region PropertyChanged Handler
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
